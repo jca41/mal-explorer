517e8c49af1839ecd107fb6fe8a9b7ede7b65c26
João Café <joao.cafe@sky.uk>
2022-04-25 22:32:24 +0100

    bg color

--- app/layouts/app.tsx
+++ app/layouts/app.tsx
@@ -5,7 +5,7 @@ type AppLayout = {
 };
 export function AppLayout({ children }: AppLayout) {
   return (
-    <div className="py-10 px-4">
+    <div className="py-10 px-4 bg-blue-100 min-h-screen">
       <div className="max-w-screen-lg mx-auto">{children}</div>
     </div>
   );
