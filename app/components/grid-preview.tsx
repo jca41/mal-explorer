92510fdfd0f65e5d28c2f0ea21253b0bfe94b147
João Café <joao.cafe@sky.uk>
2022-05-05 17:30:35 +0100

    fix img width on recs

--- app/components/grid-preview.tsx
+++ app/components/grid-preview.tsx
@@ -10,8 +10,8 @@ export function GridPreview({ children }: { children: ReactNode }) {
 export function GridPreviewItem(item: RelatedAnime | Recommendation) {
   return (
     <li className="hover:bg-blue-50 hover:shadow-md transition ease-in-out px-1 pt-1 pb-2">
-      <Link to={`/anime/${item.node.id}`}>
-        <img className="aspect-[3/4] object-cover object-top" src={item?.node?.main_picture?.large} alt={item.node.title} loading="lazy" />
+      <Link to={`/anime/${item.node.id}`} className="">
+        <img className="aspect-[3/4] object-cover object-top w-full" src={item?.node?.main_picture?.large} alt={item.node.title} loading="lazy" />
         <div className="mt-2 leading-snug tracking-tight text-sm">
           <h4 className="font-semibold">{item.node.title}</h4>
           {'relation_type_formatted' in item && <div className="text-slate-500 mt-1">{item.relation_type_formatted}</div>}
