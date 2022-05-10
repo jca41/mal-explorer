c5a0883867c677099da6fafa4f7f7143c080a797
João Café <joao.cafe@sky.uk>
2022-05-10 22:04:32 +0100

    sort rankings

--- app/components/ranking-type-select.tsx
+++ app/components/ranking-type-select.tsx
@@ -1,7 +1,29 @@
+import { useSearchParams } from '@remix-run/react';
+
 import { RankingTypeParam } from '~/contracts/mal';
 
 const RANKING_TYPES: Record<RankingTypeParam, string> = {
   all: 'All',
   airing: 'Airing',
   movie: 'Movie',
+  upcoming: 'Upcoming',
+  tv: 'TV',
+  bypopularity: 'Popularity',
+  favorite: 'Favorite',
+  ova: 'OVA',
+  special: 'Special',
 };
+
+export function RankingTypeSelect({ onSubmit }: { onSubmit: () => void }) {
+  const [params] = useSearchParams();
+
+  return (
+    <select name="sort" defaultValue={params.get('sort') ?? 'all'} onChange={() => onSubmit()}>
+      {(Object.keys(RANKING_TYPES) as (keyof typeof RANKING_TYPES)[]).map((r) => (
+        <option key={r} value={r}>
+          {RANKING_TYPES[r]}
+        </option>
+      ))}
+    </select>
+  );
+}
--- app/routes/top.tsx
+++ app/routes/top.tsx
@@ -1,20 +1,22 @@
 import { LoaderFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Form, useLoaderData, useSubmit } from '@remix-run/react';
+import { useRef } from 'react';
 
+import { RankingTypeSelect } from '~/components/ranking-type-select';
 import { SearchList, SearchListItem } from '~/components/search-list';
-import { NodeList } from '~/contracts/mal';
+import { NodeList, RankingTypeParam } from '~/contracts/mal';
 import { malService } from '~/lib/mal-service.server';
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
 
-  const q = url.searchParams.get('q');
+  const sort = url.searchParams.get('sort');
 
   return malService({
     type: 'top',
     fields: 'list',
     query: {
-      ranking_type: 'all',
+      ranking_type: (sort || 'all') as RankingTypeParam,
       limit: 20,
     },
   });
@@ -22,15 +24,23 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function TopAnime() {
   const loaderData = useLoaderData<NodeList>();
+  const submit = useSubmit();
+  const ref = useRef<HTMLFormElement>(null);
 
+  const submitForm = () => {
+    submit(ref.current);
+  };
   return (
-    <div>
+    <Form ref={ref} method="get" replace>
       <h1 className="text-center text-3xl tracking-wide mb-12">Top Anime</h1>
+      <div className="mx-auto max-w-lg mb-6 flex justify-end">
+        <RankingTypeSelect onSubmit={submitForm} />
+      </div>
       <SearchList>
         {(loaderData?.data ?? []).map(({ node }) => (
           <SearchListItem key={node.id} {...node} />
         ))}
       </SearchList>
-    </div>
+    </Form>
   );
 }
