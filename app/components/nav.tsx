15dcf18aa85a1e4390868f1ccd1bc192988429fc
João Café <joao.cafe@sky.uk>
2022-05-15 19:56:46 +0100

    disable current page link

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,20 +2,22 @@ import { NavLink } from '@remix-run/react';
 
 const NAV_ITEM_COMMON = 'font-semibold text-lg tracking-wide text-white';
 const NAV_ITEM = `${NAV_ITEM_COMMON} hover:underline`;
-const NAV_ITEM_ACTIVE = `${NAV_ITEM_COMMON} underline`;
+const NAV_ITEM_ACTIVE = `${NAV_ITEM_COMMON} underline pointer-events-none`;
 
-const renderNavItem =
-  (title: string) =>
-  ({ isActive }: { isActive: boolean }) => {
-    return <span className={isActive ? NAV_ITEM_ACTIVE : NAV_ITEM}>{title}</span>;
-  };
+const getClassName = ({ isActive }: { isActive: boolean }) => (isActive ? NAV_ITEM_ACTIVE : NAV_ITEM);
 
 export function Navigation() {
   return (
     <nav className="py-4 px-8 flex space-x-4 items-center justify-center bg-blue-500 shadow-md">
-      <NavLink to="/">{renderNavItem('Search')}</NavLink>
-      <NavLink to="/top">{renderNavItem('Top')}</NavLink>
-      <NavLink to="/seasonal">{renderNavItem('Seasonal')}</NavLink>
+      <NavLink className={getClassName} to="/">
+        Search
+      </NavLink>
+      <NavLink className={getClassName} to="/top">
+        Top
+      </NavLink>
+      <NavLink className={getClassName} to="/seasonal">
+        Seasonal
+      </NavLink>
     </nav>
   );
 }
