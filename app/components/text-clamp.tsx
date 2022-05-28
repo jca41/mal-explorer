bf3eb74432e58453170829ce43cecddab3bf722c
João Café <joao.cafe@sky.uk>
2022-05-28 19:34:36 +0100

    fix clamp for short text

--- app/components/text-clamp.tsx
+++ app/components/text-clamp.tsx
@@ -1,29 +1,27 @@
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
-import { ReactNode, useState } from 'react';
+import { ReactElement, useState } from 'react';
 
 const CLAMP_THRESHOLD = 400;
 const ICON = 'w-5 h-5';
 
-export function TextClamp({ children, text }: { children: ReactNode; text: string }) {
+export function TextClamp({ children, text }: { children: ReactElement; text: string }) {
   const [expanded, setExpanded] = useState(false);
 
-  const clampClasses = !expanded ? 'line-clamp-5' : 'line-clamp-none';
+  if (text.length < CLAMP_THRESHOLD) return children;
 
   return (
-    <div className="relative">
-      <div className={clampClasses}>{children}</div>
-      {text.length >= CLAMP_THRESHOLD && (
-        <div className="absolute -bottom-8 inset-x-0 flex justify-center">
-          <button
-            onClick={() => {
-              setExpanded((e) => !e);
-            }}
-            className="z-10 rounded-full p-1 bg-slate-100 shadow-lg"
-          >
-            {expanded ? <ChevronUpIcon className={ICON} /> : <ChevronDownIcon className={ICON} />}
-          </button>
-        </div>
-      )}
+    <div className="relative mb-4">
+      <div className={!expanded ? 'line-clamp-5' : 'line-clamp-none'}>{children}</div>
+      <div className="absolute -bottom-8 inset-x-0 flex justify-center">
+        <button
+          onClick={() => {
+            setExpanded((e) => !e);
+          }}
+          className="z-10 rounded-full p-1 bg-slate-100 shadow-lg"
+        >
+          {expanded ? <ChevronUpIcon className={ICON} /> : <ChevronDownIcon className={ICON} />}
+        </button>
+      </div>
     </div>
   );
 }
