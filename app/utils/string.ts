8ed1a3a6a8bead9b78ca57ed436802e75d53f6d8
João Café <joao.cafe@sky.uk>
2022-07-29 16:07:22 +0100

    fix solo levelling bug

--- app/utils/string.ts
+++ app/utils/string.ts
@@ -1,7 +1,7 @@
 export const capitalize = (input: string) => {
   return input.charAt(0).toUpperCase() + input.slice(1);
 };
-export const formatSnakeCase = (input: string, options?: { capitalize: boolean }) => {
+export const formatSnakeCase = (input = '', options?: { capitalize: boolean }) => {
   const shouldCapitalize = options?.capitalize ?? true;
   const split = input.split('_');
 
