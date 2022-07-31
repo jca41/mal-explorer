0fe87b856ad3d23d46b77e9fe17dec4c0a355a0f
João Café <joao.cafe@sky.uk>
2022-08-01 00:14:27 +0100

    xstate working

---
+++ app/components/my-list-status/form-machine.ts
@@ -0,0 +1,80 @@
+import { assign, createMachine } from 'xstate';
+
+import { MyListStatus } from '~/contracts/mal';
+import { isNotStatus } from '~/utils/check-data';
+
+type MContext = {
+  state: { status: MyListStatus['status']; startDate: MyListStatus['start_date'] };
+  visibleFields: {
+    score: boolean;
+    numEpisodesWatched: boolean;
+    dates: boolean;
+    finishDate: boolean;
+  };
+  numEpisodes: MyListStatus['num_episodes_watched'];
+};
+
+type MEvents = {
+  type: 'FIELD_CHANGE';
+  data: {
+    status?: MyListStatus['status'];
+    startDate?: MyListStatus['start_date'];
+  };
+};
+
+export const myListStatusFormMachine = createMachine(
+  {
+    id: 'my-list-status-form',
+    schema: {
+      events: {} as MEvents,
+      context: {} as MContext,
+    },
+    initial: 'idle',
+    states: {
+      idle: {
+        entry: 'updateVisibleFields',
+        on: {
+          FIELD_CHANGE: {
+            target: 'status',
+          },
+        },
+      },
+      status: {
+        entry: ['updateState', 'updateVisibleFields'],
+        always: {
+          target: 'idle',
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      updateState: assign({
+        state: (ctx, e) => {
+          const { type, ...eventData } = e;
+          return {
+            ...ctx.state,
+            ...eventData.data,
+          } as const;
+        },
+      }),
+      updateVisibleFields: assign({
+        visibleFields: (ctx) => {
+          const {
+            state: { status, startDate },
+            numEpisodes,
+          } = ctx;
+
+          const isNotPlanToWatch = isNotStatus(status, 'plan_to_watch');
+
+          return {
+            score: isNotPlanToWatch,
+            numEpisodesWatched: numEpisodes > 1 && isNotStatus(status, ['completed', 'plan_to_watch']),
+            dates: isNotPlanToWatch,
+            finishDate: isNotStatus(status, ['watching', 'on_hold']) && !!startDate,
+          };
+        },
+      }),
+    },
+  }
+);
