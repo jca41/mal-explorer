0a37e1844cf49bf6794cdfcb9b13074291ad6920
João Café <joao.cafe@sky.uk>
2022-08-10 14:05:10 +0100

    fix: loading size

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -7,10 +7,13 @@ export function LoadingIndication() {
   const transition = useTransition();
 
   return (
-    <Transition show={transition.state !== 'idle'} appear {...FADE_TRANSITION} className="fixed bottom-10 left-10 lg:bottom-16 lg:left-16 z-50">
-      <div className="animate-pulse">
-        <img className="w-20 animate-bounce" src="/images/kon.webp" />
-      </div>
+    <Transition
+      show={transition.state !== 'idle'}
+      appear
+      {...FADE_TRANSITION}
+      className="fixed bottom-6 left-6 sm:bottom-10 sm:left-10 lg:bottom-16 lg:left-16 z-50"
+    >
+      <img className="w-20 animate-bounce" src="/images/kon.webp" />
     </Transition>
   );
 }
