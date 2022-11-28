358fc6c20cb77b6076bf784aa6b87734a6770f12
João Café <joao.cafe@sky.uk>
2022-11-28 09:19:24 +0000

    feat: add analytics

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,8 @@
 import { RemixBrowser } from '@remix-run/react';
+import splitbee from '@splitbee/web';
 import { hydrateRoot } from 'react-dom/client';
 
 hydrateRoot(document, <RemixBrowser />);
+
+// This initiliazes Splitbee.js
+splitbee.init();
